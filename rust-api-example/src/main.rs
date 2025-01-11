5b3bcbf41fac5550f41862ecf9d56502632a37d9
olavkhn <o.kuhnen@leadinnovation.nl>
2025-01-11 15:56:07 +0100

    cleaned code, names for request in workflow/github actions

--- rust-api-example/src/main.rs
+++ rust-api-example/src/main.rs
@@ -2,7 +2,7 @@ use actix_web::{get, post, put, delete, web, App, HttpResponse, HttpServer, Resp
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
 
-// Struct voor een eenvoudige databank
+
 #[derive(Serialize, Deserialize, Clone)]
 struct Item {
     id: u32,
@@ -10,18 +10,18 @@ struct Item {
     description: String,
 }
 
-// Shared state
+
 struct AppState {
     items: Mutex<Vec<Item>>,
 }
 
-// GET: /status
+
 #[get("/status")]
 async fn status() -> impl Responder {
     HttpResponse::Ok().json(serde_json::json!({ "status": "API is running" }))
 }
 
-// POST: /items
+
 #[post("/items")]
 async fn create_item(item: web::Json<Item>, data: web::Data<AppState>) -> impl Responder {
     let mut items = data.items.lock().unwrap();
@@ -29,17 +29,17 @@ async fn create_item(item: web::Json<Item>, data: web::Data<AppState>) -> impl R
     HttpResponse::Created().finish()
 }
 
-// GET: /items
+
 #[get("/items")]
 async fn get_items(data: web::Data<AppState>) -> impl Responder {
     let items = data.items.lock().unwrap();
     HttpResponse::Ok().json(items.clone())
 }
 
-// GET: /items/{id}
+
 #[get("/items/{id}")]
 async fn get_item_by_id(id: web::Path<u32>, data: web::Data<AppState>) -> impl Responder {
-    let id = id.into_inner(); // Pak de waarde van id uit als een kopieerbare u32
+    let id = id.into_inner();
     let items = data.items.lock().unwrap();
     if let Some(item) = items.iter().find(|item| item.id == id) {
         HttpResponse::Ok().json(item)
@@ -54,7 +54,7 @@ async fn update_item(
     updated_item: web::Json<Item>,
     data: web::Data<AppState>,
 ) -> impl Responder {
-    let id = id.into_inner(); // Pak de waarde van id uit als een kopieerbare u32
+    let id = id.into_inner();
     let mut items = data.items.lock().unwrap();
     if let Some(item) = items.iter_mut().find(|item| item.id == id) {
         *item = updated_item.into_inner();
@@ -66,7 +66,7 @@ async fn update_item(
 
 #[delete("/items/{id}")]
 async fn delete_item(id: web::Path<u32>, data: web::Data<AppState>) -> impl Responder {
-    let id = id.into_inner(); // Pak de waarde van id uit als een kopieerbare u32
+    let id = id.into_inner();
     let mut items = data.items.lock().unwrap();
     if let Some(pos) = items.iter().position(|item| item.id == id) {
         items.remove(pos);
@@ -76,7 +76,6 @@ async fn delete_item(id: web::Path<u32>, data: web::Data<AppState>) -> impl Resp
     }
 }
 
-// Main functie
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let shared_data = web::Data::new(AppState {
