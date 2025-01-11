c454ba3c8a5630ac291388040ceac72859e908a6
olavkhn <o.kuhnen@leadinnovation.nl>
2025-01-11 15:46:23 +0100

    increased sleep time

--- rust-api-example/src/main.rs
+++ rust-api-example/src/main.rs
@@ -93,7 +93,7 @@ async fn main() -> std::io::Result<()> {
             .service(update_item)
             .service(delete_item)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
